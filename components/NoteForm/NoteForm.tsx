1dd43a2249052bdea18648485a99030ce4c0d618
ViktorIgnatiev <viktor.ignatiev@wp.pl>
2025-08-19 21:33:41 +0200

    cont. required delete

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -18,8 +18,7 @@ const validationSchema = Yup.object().shape({
     .max(50, 'Title must be at most 50 characters')
     .required('Title is required'),
   content: Yup.string()
-    .max(500, 'Content must be at most 500 characters')
-    .required('Content is required'),
+    .max(500, 'Content must be at most 500 characters'),
   tag: Yup.string()
     .oneOf(['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'], 'Invalid tag')
     .required('Tag is required'),
